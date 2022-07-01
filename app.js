ff42b006abf7900bfe62b7d795d8844ccd90094e
iency <pwnzjake@gmail.com>
2022-07-01 14:03:12 +0300

    JS

---
+++ app.js
@@ -0,0 +1,41 @@
+// let count = 1;
+
+// while (count <= 5) {
+//    console.log(count);
+//    count = count + 1;
+//}
+
+// for (let i = 0; i < 3; i++ /*i++ = i + 1*/) {
+//    console.log (i);
+//}
+
+
+/* for (let i = 1; i <= 20; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log(`${i} -> Frontend Simplified`)
+    }
+    else if (i % 3 === 0) {
+        console.log(`${i} -> Frontend`)
+    }
+    else if (i % 5 === 0) {
+        console.log(`${i} -> Simplified`)
+    }
+    else {
+        console.log(`${i} -> ${i}`)
+    }
+}
+*/
+
+
+// first option figured by me
+for (let i = [0]; i < 19; i++) {
+    console.log(`Frontend Simplified` [i]);
+}
+
+
+// second option tought by David 
+const str = "Frontend Simplified"
+
+for (let i = 0; i < str.length; ++i) {
+    console.log(str[i])
+}
