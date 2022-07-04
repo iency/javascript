1b273fd24a7fee1da94cfe7b620f6034377b10c6
iency <pwnzjake@gmail.com>
2022-07-04 18:56:27 +0300

    commit

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
- function lastElem(arr) {
-    return arr[arr.length - 1];
- }
+function lastElem(arr) {
+    return arr[arr.length - 1] 
+}
 
- console.log(lastElem([3, 2, 0, 6, 2]))
+console.log(lastElem([3, 2, 0, 4]))
